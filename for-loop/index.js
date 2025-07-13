7b47bb417b36d29eb2cd5a440c194266e9614f3d
Russ Allen Garde <russgarde03@gmail.com>
2025-07-13 14:21:51 +0800

    for-loop

---
+++ for-loop/index.js
@@ -0,0 +1,16 @@
+/*for(let i = 10; i > 0; i--){
+    console.log(i)
+}
+
+console.log("Happy New Year");*/
+
+for(let i = 1; i <=20; i++){
+
+    if(i == 13){
+        break;
+    }
+    else{
+        console.log(i);
+    }
+
+}
